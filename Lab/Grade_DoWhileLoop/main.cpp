67e10cfd5e3c735a2304826b1e3cd0d1bcfb6a54
rcc <rcc>
2017-01-17 11:44:41 -0800

    Signed-off-by: Jamil Batshoun

---
+++ Lab/Grade_DoWhileLoop/main.cpp
@@ -0,0 +1,54 @@
+/* 
+ * File:   main.cpp
+ * Author: Jamil Batshoun
+ * Created on January 17, 2016,11:09 AM
+ * Purpose:  Grade do while
+ */
+
+//System Libraries Here
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    
+    srand(static_cast<unsigned int>(time(0)));
+    //Declare all Variables Here
+    unsigned short score;
+    char grade, numStd = 20, student = 1;
+    unsigned short average = 0;
+    //Input or initialize values Here
+    cout<<"This program assigns the score and outputs the grade for "
+            <<static_cast<int>(numStd)<<" students"<<endl;
+    
+    //Loop for each student
+    do{
+    //Assign a grade
+        unsigned short score = rand()%50+50;
+        average+=score;
+        //process by mapping inputs to outputs
+        char grade = (score >= 90)?'A':
+            (score >= 80)?'B':
+            (score >= 70)?'C':
+            (score >= 60)?'D':'F';
+    //Output Located Here
+        cout<<"Student "<<static_cast<int>(student)<<" has score "<<score
+                <<" = "<<grade<<endl;
+        
+    }while(++student <= numStd);
+    average/=numStd;
+    cout<<"The average test score = "<<average<<endl;
+    
+    //Exit
+    return 0;
+}
+
