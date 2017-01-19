7ac7d82c8a6307c7e36d9870497fd013fc2fd430
rcc <rcc>
2017-01-19 13:56:28 -0800

    Signed-off-by: Jamil Batshoun

---
+++ Hmwk/C++Template/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Jamil Batshoun
+ * Created on January 19, 2017, 1:35 PM
+ * Purpose: 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input values
+    
+    //Process by mapping inputs to outputs
+    
+    //Output values
+
+    //Exit stage right!
+    return 0;
+}
